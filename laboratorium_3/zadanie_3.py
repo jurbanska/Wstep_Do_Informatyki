035d68b94467c0c0e231e5945618f0defacfec47
jurbanska <jurbanska@student.agh.edu.pl>
2021-10-26 13:32:21 +0200

    variables added

---
+++ laboratorium_3/zadanie_3.py
@@ -0,0 +1,5 @@
+age = 19
+number = 6.5
+number_2 = 5.25
+math = 'algebra'
+math_2 = 'analiza'
---
+++ laboratorium_3/zadanie_4.py
@@ -0,0 +1,8 @@
+name = 'Julia'
+surname = 'Urbanska'
+age = 19
+favourite_dish = 'Placki ziemniaczane'
+favourite_animal = 'Pies'
+dzielenie = 5/7
+
+print
