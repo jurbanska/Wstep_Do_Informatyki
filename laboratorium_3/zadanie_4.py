d7f905c91361073366322a3900c817d33780f4ab
jurbanska <jurbanska@student.agh.edu.pl>
2021-10-26 13:53:04 +0200

    task done

--- laboratorium_3/zadanie_4.py
+++ laboratorium_3/zadanie_4.py
@@ -3,6 +3,25 @@ surname = 'Urbanska'
 age = 19
 favourite_dish = 'Placki ziemniaczane'
 favourite_animal = 'Pies'
-dzielenie = 5/7
+divide = 5/7
 
-print
+print('Imie: ' + name)
+print('Nazwisko: ' + surname)
+print('Wiek: ', age)
+print('Ulubiona potrawa: ' + favourite_dish)
+print('Ulubione zwierze: ' + favourite_animal)
+print('Dzielenie: ',  divide)
+
+print(name, surname, age,favourite_dish, favourite_animal, divide)
+
+print(round(divide,1))
+print(round(divide,3))
+print(round(divide,5))
+print(round(divide,10))
+
+print('Dane: ' 'Julia', 'Urbanska', '19', 'placki ziemniaczane', 'pies')
+
+print(round(5/7,1))
+print(round(5/7,3))
+print(round(5/7,5))
+print(round(5/7,10))
