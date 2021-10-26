e22cf6375a899ebdd15e67d628b9dcb205774698
jurbanska <jurbanska@student.agh.edu.pl>
2021-10-26 14:20:54 +0200

    a) done

---
+++ laboratorium_3/zadanie_6.py
@@ -0,0 +1,20 @@
+l1 = int(input('Podaj liczbe nr1: '))
+l2 = int(input('Podaj liczbe nr2: '))
+
+suma = l1 + l2 ## dodawanie liczby l1 i l2
+print('Suma: ', suma)
+
+roznica = l1 - l2 ## odejmowanie liczby l1 i l2
+print('Roznica: ', roznica)
+
+iloczyn = l1*l2 ## mnozenie liczby l1 i l2
+print('Iloczyn: ', iloczyn)
+
+iloraz = l1/l2 ## dzielenie liczby l1 i l2
+print('Iloraz: ', iloraz)
+
+kwadrat_l1 = l1*l1
+print('Kwadrat l1: ', kwadrat_l1)
+
+kwadrat_l2 = l2*l2
+print('Kwadrat l2: ', kwadrat_l2)
