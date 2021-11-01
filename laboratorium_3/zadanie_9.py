705e0c72962ede1fd7aa40bd2de1bc29b392ed26
jurbanska <jurbanska@student.agh.edu.pl>
2021-11-01 13:58:17 +0100

    task done

--- laboratorium_3/zadanie_9.py
+++ laboratorium_3/zadanie_9.py
@@ -1,22 +1,22 @@
 operacja = input('Co chcesz zrobic w kolejnym kroku - wplata/wyplata/saldo/zakoncz: ')
-pin = input('Podaj pin: ') #pin ustawiony na 1234
+pin = input('Podaj pin: ')  # pin ustawiony na 1234
 saldo = 0
 
 while operacja != 'zakoncz':
     if pin == '1234':
-            if operacja == 'wplata':
-                wplata = int(input('Podaj wplacana kwote: '))
-                saldo = saldo + wplata
+        if operacja == 'wplata':
+            wplata = int(input('Podaj wplacana kwote: '))
+            saldo = saldo + wplata
 
-            if operacja == 'wyplata':
-                wyplata = int(input('Podaj wyplacana kwote: '))
-                if wyplata <= saldo:
-                    saldo = saldo - wyplata
-                else:
-                    print('Nie posiadasz wystarczajacych srodkow na koncie, twoje saldo to: ', saldo)
+        if operacja == 'wyplata':
+            wyplata = int(input('Podaj wyplacana kwote: '))
+            if wyplata <= saldo:
+                saldo = saldo - wyplata
+            else:
+                print('Nie posiadasz wystarczajacych srodkow na koncie, twoje saldo to: ', saldo)
 
-            if operacja == 'saldo':
-                print('Saldo: ', saldo)
+        if operacja == 'saldo':
+            print('Saldo: ', saldo)
     else:
         print('Nieprawidlowy PIN')
 
