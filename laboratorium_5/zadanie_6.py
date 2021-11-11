112d34d856dfebd4fe2d64c40c2b92374cfbd942
jurbanska <jurbanska@student.agh.edu.pl>
2021-11-11 19:17:10 +0100

    more efficient

--- laboratorium_5/zadanie_6.py
+++ laboratorium_5/zadanie_6.py
@@ -4,7 +4,7 @@ if liczba < 2:
     print("Liczba nie jest pierwsza")
     exit()
 
-for i in range(2, liczba): #aby algorytm byl bardziej wydajny, iteracje mozna wykonywac do pierwiastka z liczby
+for i in range(2, (liczba ** (1/2)) + 1):
     if (liczba % i) == 0:
         print("Liczba jest złożona")
         exit()
