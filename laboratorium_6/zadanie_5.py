306c55b746aa46628565ba59c00816c43f06d4eb
jurbanska <jurbanska@student.agh.edu.pl>
2021-11-11 20:17:06 +0100

    palindrome (binary system) added

--- laboratorium_6/zadanie_5.py
+++ laboratorium_6/zadanie_5.py
@@ -3,24 +3,37 @@ i = 0
 j = len(liczba) - 1
 
 if len(liczba) == 1:
-    print("Liczba jest palindromem")
+    print("Liczba jest palindromem w systemie dziesietnym")
 
-if len(liczba) % 2 == 0:  # jesli liczba ma parzysta ilosc cyfr oraz dzieli sie przez 11, to w systemie dziesietnym jest palindromem
+if len(liczba) % 2 == 0:  # jesli liczba ma parzysta ilosc cyfr i dzieli sie przez 11, to w dziesietnym jest palindromem
     if int(liczba) % 11 == 0:
-        print("Liczba jest palindromem")
+        print("Liczba jest palindromem w systemie dziesietnym")
     else:
-        print("Liczba nie jest palindromem")
+        print("Liczba nie jest palindromem w systemie dziesietnym")
 else:
     while i < len(liczba) / 2 < j:
         if liczba[i] == liczba[j]:
             i += 1
             j -= 1
-            print("Liczba jest palindromem")
-            exit()
+            print("Liczba jest palindromem w systemie dziesietnym")
+            break
         else:
-            print("Liczba nie jest palindromem")
-            exit()
+            print("Liczba nie jest palindromem w systemie dziesietnym")
+            break
 
+binarnie = bin(int(liczba))[2:]  # ucinam przedrostek '0b', gdyz python tak zapisuje l. binarna
+i = 0
+j = len(binarnie) - 1
 
+if len(binarnie) == 1:
+    print("Liczba jest palindromem w systemie binarnym")
 
-#binarnie = bin(int(liczba))[2:] #ucinam przedrostek '0b', gdyz python tak zapisuje l. binarna
+while i < len(binarnie) / 2 < j:
+    if binarnie[i] == binarnie[j]:
+        i += 1
+        j -= 1
+        print("Liczba jest palindromem w systemie binarnym")
+        break
+    else:
+        print("Liczba nie jest palindromem w systemie binarnym")
+        break
