a2139c133478ddf6ea17cca913284a9d159e4508
jurbanska <jurbanska@student.agh.edu.pl>
2021-11-30 12:35:36 +0100

    comments and exceptions added

--- laboratorium_8/zadanie_1.py
+++ laboratorium_8/zadanie_1.py
@@ -1,29 +1,39 @@
 # Napisz program, który utworzy i wypisze listę składającą się z N liczb z przedziału [1,100], a następnie po każdej
 # liczbie pierwszej wstawi nowy element o wartości 0. Następnie program powinien policzyć sumy podzbiorów
 # znajdujących się pomiędzy zerami. Lista przed modyfikacją: [7,45,45,34,53,45,4,3,11,18]. Lista po modyfikacji:
-# [7,0,45,45,34,53,0,45,4,3,0,11,0,18,30]. Wynik: 7;177;52;11;48
+# [7,0,45,45,34,53,0,45,4,3,0,11,0,18]. Wynik: 7;177;52;11;18
 import random
+
+
+class NegativeNumberError(Exception):
+    pass
 
 
 def prime(liczba):  # funkcja sprawdzajaca, czy liczba jest pierwsza
     if liczba < 2:
-        return 1
+        return 1  # liczba nie jest pierwsza
 
     for i in range(2, liczba):
         if (liczba % i) == 0:
             return 1
-    return 0
+    return 0  # liczba jest pierwsza
 
 
 i = 0
 liczby = []
-N = int(input("Długość listy: "))
 
-while i < N:  # generowanie listy z losowymi liczbami z przedziały [1,100]
+try:
+    N = int(input("Długość listy: "))
+    if N <= 0:
+        raise NegativeNumberError
+except (ValueError, NegativeNumberError):
+    print("Liczba nie jest całkowita dodatnia")
+    exit()
+
+while i < N:  # generowanie listy z losowymi liczbami z przedziału [1,100]
     liczby.insert(i, random.randint(1, 100))
     i += 1
 print(liczby)
-
 
 i = 0
 while i < len(liczby):  # wstawianie 0 po każdej liczbie pierwszej
@@ -40,13 +50,14 @@ if liczby[len(liczby) - 1] != 0:
 suma = 0
 wynik = []
 
-for i in range(0, len(liczby)):
+for i in range(0, len(liczby)):  # sumowanie podzbiorów między zerami
     suma = liczby[i] + suma
     if liczby[i] == 0:
         wynik.append(suma)
         suma = 0
 
-liczby.pop(len(liczby) - 1)
+if liczby[len(liczby) - 1] == 0:
+    liczby.pop(len(liczby) - 1)
 print(wynik)
 
 '''
